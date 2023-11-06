3bbf6c7e4b044b0804f0e21c976418b035d6d0b0
Luis Irausquin <luis18.ei95@gmail.com>
2023-11-05 20:05:22 -0400

    debug

--- delete_repeated_numbers.py
+++ delete_repeated_numbers.py
@@ -2,14 +2,11 @@
 Remove repeated numbers from a list
 """
 
-numbers = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9, 9]
-numbers_repeated = []
+numbers = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9, 1, 2, 2, 2, 3, 3]
 
+unique_numbers = []
 for number in numbers:
-    check = number in numbers_repeated
-    if number == check:
-        continue
-    else:
-        del numbers[number]
+    if number not in unique_numbers:
+        unique_numbers.append(number)
 
-print(numbers)
+print(unique_numbers)
